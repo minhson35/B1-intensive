28de4fcb512d67287a648e295e874a0383df90ef
Lê Minh Sơn <leminhson@Sons-MacBook-Pro.local>
2024-04-21 09:55:41 +0700

    update

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function add(event){
     event.preventDefault(); 
+    let img = document.getElementById('input_image').value;
     let title = document.getElementById('input_title').value;
     let author = document.getElementById('input_author').value;
-    console.log(title, author);
+    console.log(img, title, author);
 
     let userData = JSON.parse(localStorage.getItem('userData')) || [];
     let isExisting = userData.some();
